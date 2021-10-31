7cc59d1039386fa8e5fe2666b67d03332eba3a6a
Tejas Ozarkar <tejasozarkar@Tejass-MacBook-Pro.local>
2021-10-31 18:09:33 +0530

    FEATURE: Voxeet SDK Added to Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import VoxeetSDK from '@voxeet/voxeet-web-sdk';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { AuthProvider } from '../context/AuthContext';
 import PrivateRoute from '../util/PrivateRoute';
@@ -6,6 +8,9 @@ import Login from './Login';
 import Signup from './Signup';
 
 function App() {
+  useEffect(() => {
+    VoxeetSDK.initialize(process.env.REACT_APP_DOLBY_CUSTOMER_KEY, process.env.REACT_APP_DOLBY_CUSTOMER_SECRET);
+  }, []);
   return (
     <div className="App">
       <Router>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,33 @@
 import Button from '@restart/ui/esm/Button';
-import React from 'react'
+import VoxeetSDK from '@voxeet/voxeet-web-sdk';
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router';
 import { useAuth } from '../context/AuthContext'
 
 const Dashboard = () => {
     const { currentUser, logout } = useAuth();
     const history = useHistory();
+
+    const [conferenceName, setConferenceName] = useState('');
+
+    useEffect(() => {
+        VoxeetSDK.conference.on('streamAdded', (participant, stream) => {
+            if (stream.type === 'ScreenShare') return;
+            addParticipantNode(participant);
+            if (stream.getVideoTracks().length) {
+                addVideoNode(participant, stream);
+            }
+        })
+        VoxeetSDK.conference.on('streamUpdated', (participant, stream) => {
+            if (stream.type === 'ScreenShare') return;
+            addParticipantNode(participant);
+            if (stream.getVideoTracks().length) {
+                addVideoNode(participant, stream);
+            }
+        });
+    }, []);
+
+
     const handleLogout = async () => {
         try {
             await logout();
@@ -14,10 +36,62 @@ const Dashboard = () => {
 
         }
     }
+
+    const handleCreateConference = async () => {
+        await VoxeetSDK.session.open({ name: currentUser.email })
+        VoxeetSDK.conference.create({ alias: conferenceName })
+            .then(conference => {
+                VoxeetSDK.conference.join(conference, {})
+                    .then()
+            })
+    }
+
+    const handleStartVideo = () => {
+        VoxeetSDK.conference.startVideo(VoxeetSDK.session.participant)
+            .then();
+    }
+
+    const addVideoNode = (participant, stream) => {
+        console.log('adding video node)')
+        let videoNode = document.getElementById('video-' + participant.id);
+        if (!videoNode) {
+            console.log('createing video nodee')
+            videoNode = document.createElement('video');
+            videoNode.setAttribute('id', 'video-' + participant.id);
+            videoNode.setAttribute('height', 240);
+            videoNode.setAttribute('width', 320);
+            videoNode.setAttribute("playsinline", true);
+            videoNode.muted = true;
+            videoNode.setAttribute("autoplay", 'autoplay');
+
+            const videoContainer = document.getElementById('video-container');
+            videoContainer.appendChild(videoNode);
+        }
+        navigator.attachMediaStream(videoNode, stream);
+    }
+
+    const addParticipantNode = (participant) => {
+        let participantNode = document.getElementById('participant-' + participant.info.name);
+        if (!participantNode) {
+            const participantContainer = document.getElementById('participant-list');
+            participantNode = document.createElement('li');
+            participantNode.innerText = participant.info.name;
+            participantNode.setAttribute('id', 'participant-' + participant.info.name);
+            participantContainer.append(participantNode);
+        }
+    }
+
     return (
         <>
             <h3> Welcome {currentUser && currentUser.email} </h3>
             <Button className="btn btn-default" onClick={handleLogout}>Logout</Button>
+            <input type="text" onChange={(e) => setConferenceName(e.target.value)}></input>
+            <Button className="btn btn-primary" onClick={handleCreateConference}>Create Conference</Button>
+            <Button className="btn btn-success" onClick={handleStartVideo}>Start Video</Button>
+            <div id="video-container"></div>
+            <div id='participant-list'>
+
+            </div>
         </>
     )
 }

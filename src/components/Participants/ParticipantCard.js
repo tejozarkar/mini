c938e09dd0e45ce5366945e3db5c2846d9ae034b
Tejas Ozarkar <tejozarkar@gmail.com>
2021-11-25 15:43:28 +0530

    FIX Null Pointer Error for Participant Card

--- src/components/Participants/ParticipantCard.js
+++ src/components/Participants/ParticipantCard.js
@@ -8,13 +8,17 @@ const ParticipantCard = ({ participant, userId }) => {
     const { admins } = useConference();
 
     return (
-        <div className="participant-card p-3 m-auto d-flex justify-content-center align-items-center" style={{ background: getAlphabetColor(participant.name[0]) }} id={`video-container-${participant.id}`}>
-            <p style={{ marginBottom: '0', fontSize: '2em' }}>{toTitleCase(participant.name)}</p>
-            <div className="overlay px-2 py-1">
-                {toTitleCase(participant.name)}
-            </div>
-            {admins && admins.hasOwnProperty(userId) && <Tag color="#87d068" className="border admin-tag">Admin</Tag>}
-        </div>
+        <>
+            {participant &&
+                <div className="participant-card p-3 m-auto d-flex justify-content-center align-items-center" style={{ background: getAlphabetColor(participant.name[0]) }} id={`video-container-${participant.id}`}>
+                    <p style={{ marginBottom: '0', fontSize: '2em' }}>{toTitleCase(participant.name)}</p>
+                    <div className="overlay px-2 py-1">
+                        {toTitleCase(participant.name)}
+                    </div>
+                    {admins && admins.hasOwnProperty(userId) && <Tag color="#87d068" className="border admin-tag">Admin</Tag>}
+                </div>
+            }
+        </>
     )
 }
 
